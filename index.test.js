28752da6cd61c42e7fb82cd5f5b2451a78ddae06
zacanger <zac@zacanger.com>
2018-09-15 10:59:01 -0600

    Add a test

---
+++ index.test.js
@@ -0,0 +1,17 @@
+const tape = require('tape')
+const header = require('./')
+
+const ctx = {
+  set (type, data) {
+    this[type] = data
+  }
+}
+
+const next = () =>
+  Promise.resolve(1)
+
+tape.test('header', (t) => {
+  header('foo', 'bar')(ctx, next)
+  t.equal(ctx.foo, 'bar')
+  t.end()
+})
