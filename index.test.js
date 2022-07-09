e9db51671f851859f20586a40086711125e3884d
zacanger <zac@zacanger.com>
2022-07-09 17:17:09 -0600

    chore: updates

--- index.test.js
+++ index.test.js
@@ -1,12 +1,10 @@
-// eslint-disable-next-line node/no-unpublished-require
 const tape = require('tape')
 const header = require('.')
 
 const ctx = {
-  set(type, data) {
-    // eslint-disable-next-line fp/no-this
+  set (type, data) {
     this[type] = data
-  },
+  }
 }
 
 const next = () => Promise.resolve(1)
